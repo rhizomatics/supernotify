9d1ea03999e6b8add5df77340fe63dc82139c517
Jey Burrows <jrb@jeymail.net>
2025-12-11 00:39:05 +0000

    fix broken pycares/aiodns deps

--- refresh-deps.sh
+++ refresh-deps.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 echo "Checking for pip update ..."
 uv pip install --upgrade pip
-echo "Updating core requirements ..."
-uv pip install -qr requirements.txt --upgrade
-echo "Updating test requirements ..."
-uv pip install -qr requirements_test.txt --upgrade
+
 echo "Updating docs requirements ..."
 uv pip install -qr requirements_docs.txt --upgrade
+
+echo "Updating test requirements ..."
+uv pip install -qr requirements_test.txt --upgrade
+
+echo "Updating core requirements ..."
+
+uv pip install -qr requirements.txt --upgrade
+
+
 echo "Pre-commit autoupdate ..."
 pre-commit autoupdate
