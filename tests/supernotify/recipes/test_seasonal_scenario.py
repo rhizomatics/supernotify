ef27f321ea68fd67c9b58c17e8f7bd3394ef4569
Jey Burrows <jrb@jeymail.net>
2025-12-11 18:01:46 +0000

    lint fix

--- tests/supernotify/recipes/test_seasonal_scenario.py
+++ tests/supernotify/recipes/test_seasonal_scenario.py
@@ -10,7 +10,7 @@ from tests.supernotify.hass_setup_lib import TestingContext, register_device
 
 @pytest.fixture
 async def recipe_fixture(hass: HomeAssistant):
-  ctx = TestingContext(
+    ctx = TestingContext(
         homeassistant=hass,
         yaml="""
 transport:
@@ -54,57 +54,57 @@ scenarios:
         transport_types=[ChimeTransport],
         services={"alexa_devices": ["send_sound"]},
     )
-  register_device(
-        ctx.hass_api, device_id="00001111222233334444555566667777",
-                      domain="alexa_devices", domain_id="test_01", title="test fixture"
+    register_device(
+        ctx.hass_api,
+        device_id="00001111222233334444555566667777",
+        domain="alexa_devices",
+        domain_id="test_01",
+        title="test fixture",
     )
-  await ctx.test_initialize()
-  return ctx
+    await ctx.test_initialize()
+    return ctx
 
 
 @pytest.mark.freeze_time(dt.datetime(2024, 12, 25, 11, tzinfo=dt.UTC))
 async def test_seasonal_scenario_on_xmas_day(recipe_fixture):
 
-    uut = Notification(recipe_fixture, "testing 123", action_data={
-                       "delivery": "doorbell_rang"})
+    uut = Notification(recipe_fixture, "testing 123", action_data={"delivery": "doorbell_rang"})
     await uut.initialize()
     await uut.deliver()
 
     assert len(uut.delivered_envelopes) == 1
     assert len(uut.delivered_envelopes[0].calls) == 1
     call = uut.delivered_envelopes[0].calls[0]
-    assert call.action == 'send_sound'
-    assert call.domain == 'alexa_devices'
-    assert call.action_data['sound'] == 'christmas_05'
+    assert call.action == "send_sound"
+    assert call.domain == "alexa_devices"
+    assert call.action_data["sound"] == "christmas_05"
 
 
 @pytest.mark.freeze_time(dt.datetime(2024, 12, 25, 11, tzinfo=dt.UTC))
 async def test_unseasonal_scenario_on_xmas_day(recipe_fixture):
 
-    uut = Notification(recipe_fixture, "testing 123", action_data={
-                       "delivery": "driveway_alarm"})
+    uut = Notification(recipe_fixture, "testing 123", action_data={"delivery": "driveway_alarm"})
     await uut.initialize()
     await uut.deliver()
 
     assert len(uut.delivered_envelopes) == 1
     assert len(uut.delivered_envelopes[0].calls) == 1
     call = uut.delivered_envelopes[0].calls[0]
-    assert call.action == 'send_sound'
-    assert call.domain == 'alexa_devices'
-    assert call.action_data['sound'] == 'amzn_sfx_trumpet_bugle_04'
+    assert call.action == "send_sound"
+    assert call.domain == "alexa_devices"
+    assert call.action_data["sound"] == "amzn_sfx_trumpet_bugle_04"
 
 
 @pytest.mark.freeze_time(dt.datetime(2024, 6, 25, 11, tzinfo=dt.UTC))
 async def test_seasonal_scenario_in_summer(recipe_fixture):
 
-    uut = Notification(recipe_fixture, "testing 123", action_data={
-                       "delivery": "doorbell_rang"})
+    uut = Notification(recipe_fixture, "testing 123", action_data={"delivery": "doorbell_rang"})
     await uut.initialize()
     await uut.deliver()
 
     assert len(uut.delivered_envelopes) == 1
     assert len(uut.delivered_envelopes[0].calls) == 1
     call = uut.delivered_envelopes[0].calls[0]
-    assert call.action == 'send_sound'
-    assert call.domain == 'alexa_devices'
-    assert call.action_data['sound'] == 'amzn_sfx_doorbell_chime_02'
+    assert call.action == "send_sound"
+    assert call.domain == "alexa_devices"
+    assert call.action_data["sound"] == "amzn_sfx_doorbell_chime_02"
