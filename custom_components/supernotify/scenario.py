b5737c07d9c966c456f41019024a6b12effb635c
Jey Burrows <jrb@jeymail.net>
2025-12-22 13:04:20 +0000

    scenario log

--- custom_components/supernotify/scenario.py
+++ custom_components/supernotify/scenario.py
@@ -116,7 +116,7 @@ class Scenario:
                     if re.fullmatch(name_or_pattern, delivery_name):
                         if self._delivery_selector.get(delivery_name) == delivery_name:
                             _LOGGER.info(
-                                f"SUPERNOTIFY Scenario ignoring '{name_or_pattern}' shadowing explicit delivery {delivery_name}"
+                                f"SUPERNOTIFY Scenario {self.name} ignoring '{name_or_pattern}' shadowing explicit delivery {delivery_name}"  # noqa: E501
                             )
                         else:
                             _LOGGER.debug(f"SUPERNOTIFY Scenario delivery '{name_or_pattern}' matched {delivery_name}")
