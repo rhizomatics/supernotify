a8b05b02d7910df0a71753dc7e7f5144975c6716
Jey Burrows <jrb@jeymail.net>
2025-12-29 23:24:35 +0000

    ruff ruff

--- docgen/transport_table.py
+++ docgen/transport_table.py
@@ -13,6 +13,7 @@ def esc(v: Any) -> str:
     v = str(v) if not isinstance(v, str) else v
     return v.replace("|", "&#124;")
 
+
 def transport_doc() -> None:
     doc_filename = "developer/transports.md"
     option_keys = []
@@ -24,12 +25,12 @@ def transport_doc() -> None:
     with mkdocs_gen_files.open(doc_filename, "w") as df:
         df.write("# Transport Configuration\n\n")
         df.write("See the [Options Table](../transports/index.md/#table-of-options) for a description of each option.\n\n")
-        
+
         df.write("## Default Selection\n")
 
         df.write("|Transport|Rank|Target Required|Auto Default Delivery|Features|\n")
         df.write("|---------|----|---------------|---------------------|--------|\n")
-        for transport_class in sorted(TRANSPORTS,key=lambda t:t.name):
+        for transport_class in sorted(TRANSPORTS, key=lambda t: t.name):
             transport = transport_class(Mock(template_path=Path()))
             features: list[str] = [f.name for f in transport.supported_features]
             df.write(f"|[{transport.name}](../transports/{transport.name}.md)")
@@ -43,18 +44,18 @@ def transport_doc() -> None:
 
         df.write(f"|Transport|{'|'.join(option_keys)}|\n")
         df.write(f"|---------|{'-------|' * len(option_keys)}\n")
-        for transport_class in sorted(TRANSPORTS,key=lambda t:t.name):
+        for transport_class in sorted(TRANSPORTS, key=lambda t: t.name):
             transport = transport_class(Mock(template_path=Path()))
             options = transport.default_config.delivery_defaults.options
             df.write(f"|[{transport.name}](../transports/{transport.name}.md)")
-            df.write(f"|{'|'.join(esc(options.get(k,"N/A")) for k in option_keys)}|\n")
+            df.write(f"|{'|'.join(esc(options.get(k, 'N/A')) for k in option_keys)}|\n")
 
         df.write("\n")
         df.write("## Automatic Device Discovery\n")
 
         df.write("|Transport|Device Discovery|Device Domain|Device Model Exclude|\n")
         df.write("|---------|----------------|-------------|--------------------|\n")
-        for transport_class in sorted(TRANSPORTS,key=lambda t:t.name):
+        for transport_class in sorted(TRANSPORTS, key=lambda t: t.name):
             transport = transport_class(Mock(template_path=Path()))
             if transport.default_config.device_discovery:
                 transport = transport_class(Mock(template_path=Path()))
@@ -64,7 +65,6 @@ def transport_doc() -> None:
                 df.write(f"|{','.join(transport.default_config.device_model_exclude or [])}|\n")
 
         df.write("\n")
-        
 
 
 transport_doc()
