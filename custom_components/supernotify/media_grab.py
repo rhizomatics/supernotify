a5e7358a8d32ef43bd69e9a6bd6d78b56dd52083
Jey Burrows <jrb@jeymail.net>
2026-09-21 14:44:55 +0100

    test fix

--- custom_components/supernotify/media_grab.py
+++ custom_components/supernotify/media_grab.py
@@ -251,7 +251,7 @@ def select_avail_camera(hass_api: HomeAssistantAPI, cameras: dict[str, Any], cam
     if preferred_cam is None:
         # no alternatives for an unconfigured entity, whether camera or image, so it's just
         # a matter of whether it's known to be unavailable - assume it's fine if it has no state
-        if True:
+        if camera_available(hass_api, {CONF_CAMERA: camera_entity_id}, non_entity=True):
             return camera_entity_id
         return None
     if camera_available(hass_api, preferred_cam):
